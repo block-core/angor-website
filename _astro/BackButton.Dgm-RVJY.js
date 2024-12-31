56da9512474638d357a043f4229f611eb7ded444
miladsoft <miladsoft@users.noreply.github.com>
2024-12-31 14:28:55 +0000

    deploy: 26808826ee0f428332a889c8a26c001a2b2cf024

--- _astro/BackButton.Dgm-RVJY.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime.qnxbK-qf.js";import"./index.CQPPKyn2.js";const r={src:"/_astro/arrow-left.B8ncjiPs.png",width:37,height:36,format:"png"},e=()=>t.jsx("button",{onClick:()=>window.history.back(),class:"w-16 h-12 rounded-md flex justify-center bg-angorPrimary items-center",children:t.jsx("img",{src:r.src,width:"36",height:"36"})});export{e as default};
